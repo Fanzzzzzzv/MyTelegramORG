faaddc253bfc57c1b55dc1d0fa74683b60f41084
Fanzzzzzzv <100845326+Fanzzzzzzv@users.noreply.github.com>
2022-03-05 00:41:22 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,7 @@
 class Translation(object):
     START_TEXT = """<b>Halo Nyed!</b>\nI Gua Bisa Ambil Telegram APP ID & API Hash Dari my.telegram.org Biar Gampang !! \nGausah Login Ke Web Kesini Aja\n\n<b>Cuma Kirim Nomor Hape Terus \nProses.</b> \n <b>Nomor Nya Harus Pake + Tod.</b>\n<b>Contoh: +628XXXXXXXX</b>"""
     AFTER_RECVD_CODE_TEXT = """Tuh Tod Ambil Kode Yang Dikirim Telegram!."""
-    BEFORE_SUCC_LOGIN = "Recieved Code. Scarpping Web Page ..."
+    BEFORE_SUCC_LOGIN = "Kode Diterima,Ngehack Akun Lu , Canda Tod Wkwkwk! ..."
     ERRED_PAGE = "<b>Gagal Ngambil App ID Ambil Manual Aja Nyed!.</b> \n\n<b>@SDBOTs_inifinity</b>"
     CANCELLED_MESG = "<b>Bye! Please Reply /start the bot conversation</b>"
     IN_VALID_CODE_PVDED = "<b>Ulang Tod Kodenya Salah!</b>"
