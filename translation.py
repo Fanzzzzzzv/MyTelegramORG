ac162ef871dda83f7b98b669b4330f20133a5e25
Fanzzzzzzv <100845326+Fanzzzzzzv@users.noreply.github.com>
2022-03-05 00:40:40 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,8 +1,8 @@
 class Translation(object):
-    START_TEXT = """<b>Hi Sir!</b>\nI can get your Telegram APP ID & API Hash from my.telegram.org !! \nNo need to go to website & login there. Just Login with me.\n\n<b>Just Send me your Telegram Account's Phone Number to start \nprocess.</b> \n <b>Phone Number should be in International Format.</b>\n<b>Example: +9405748292828</b>"""
-    AFTER_RECVD_CODE_TEXT = """A Verification Code sent to you by Telegram. Enter that Verification Code Or Forward That Verification Message."""
+    START_TEXT = """<b>Halo Nyed!</b>\nI Gua Bisa Ambil Telegram APP ID & API Hash Dari my.telegram.org Biar Gampang !! \nGausah Login Ke Web Kesini Aja\n\n<b>Cuma Kirim Nomor Hape Terus \nProses.</b> \n <b>Nomor Nya Harus Pake + Tod.</b>\n<b>Contoh: +628XXXXXXXX</b>"""
+    AFTER_RECVD_CODE_TEXT = """Tuh Tod Ambil Kode Yang Dikirim Telegram!."""
     BEFORE_SUCC_LOGIN = "Recieved Code. Scarpping Web Page ..."
-    ERRED_PAGE = "<b>Something Went Wrong. Failed To Get App ID.</b> \n\n<b>@SDBOTs_inifinity</b>"
+    ERRED_PAGE = "<b>Gagal Ngambil App ID Ambil Manual Aja Nyed!.</b> \n\n<b>@SDBOTs_inifinity</b>"
     CANCELLED_MESG = "<b>Bye! Please Reply /start the bot conversation</b>"
-    IN_VALID_CODE_PVDED = "<b>Sorry, But The Input Does Not Seem To Be A Valid Telegram Web-Login Code</b>"
-    IN_VALID_PHNO_PVDED = "<b>Sorry, But The Input Does Not Seem To Be A Valid Phone Number</b>"
+    IN_VALID_CODE_PVDED = "<b>Ulang Tod Kodenya Salah!</b>"
+    IN_VALID_PHNO_PVDED = "<b>Gak Ngerti Konsol Kirim Nomor Hp Lu !</b>"
